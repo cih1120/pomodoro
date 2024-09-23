5d2eb3c078021bc6e7992b7520feb1be92da3f80
HannahWu <a22483268@gmail.com>
2024-09-23 22:23:09 +0800

    feat: Add base field to vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: [{ find: '@', replacement: '/src' }],
-  },
-  plugins: [react()],
+    base: '/promodoro/',
+    resolve: {
+        alias: [{ find: '@', replacement: '/src' }],
+    },
+    plugins: [react()],
 })
